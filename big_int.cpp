6986e6d4aaa7e280c45c160ca092d854f4089be1
Pedro Henrique Serafim <serafaobr@gmail.com>
2020-12-02 11:20:31 -0300

    Add basic API for BigInteger.

--- big_int.cpp
+++ big_int.cpp
@@ -2,7 +2,23 @@
 
 #include <iostream>
 
+using namespace std;
+
 class BigInteger {
     public:
+    // Basic Operators
+    BigInteger operator+(const BigInteger&);
+    BigInteger operator-(const BigInteger&);
+    BigInteger operator*(const BigInteger&);
+    BigInteger operator/(const BigInteger&);
+    // Advanced Operators
+    BigInteger root(int);
+    BigInteger sqrt();
+    BigInteger pow(int);
+    
     private:
+    
 };
+
+// Overload cout << operation
+ostream& operator<<(ostream& os, BigInteger const& number) {}
